b84503e08ddfd120ef9b2503837e5d182b03bfab
Kurt Gusbeth <info@quizpalme.de>
2019-04-17 23:15:54 +0200

    version 8.4.1: fix for TYPO3 9

--- Classes/ViewHelpers/ContentViewHelper.php
+++ Classes/ViewHelpers/ContentViewHelper.php
@@ -1,6 +1,8 @@
 <?php
 namespace Quizpalme\Camaliga\ViewHelpers;
 
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
+
 /**
  * Content-ViewHelper
  * {namespace cam=Quizpalme\Camaliga\ViewHelpers}
@@ -9,7 +11,8 @@ namespace Quizpalme\Camaliga\ViewHelpers;
  *
  * @package camaliga
  */
-class ContentViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+class ContentViewHelper extends AbstractTagBasedViewHelper
+{
 	/**
 	 * As this ViewHelper renders HTML, the output must not be escaped.
 	 *
@@ -25,52 +28,57 @@ class ContentViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHel
 	 */
 	protected $contentRepository = NULL;
 	
+	public function initializeArguments()
+	{
+		parent::initializeArguments();
+		$this->registerUniversalTagAttributes();
+		$this->registerTagAttribute('param', 'string', 'Parameter of the tag');
+	}
 	
-    /**
-     * Content-ViewHelper
-     * 
-     * @param string $param
-     * @return string $result
-     */
-	public function render($param) {
-		if ($_GET['tx_camaliga_pi1'])
-			$camaligaArray = $_GET['tx_camaliga_pi1'];
-		else if ($_POST['tx_camaliga_pi1'])
-			$camaligaArray = $_POST['tx_camaliga_pi1'];
-		else
-			$camaligaArray = array();
-		$uid = intval($camaligaArray['content']);
-		//$lang = intval($_GET['L']);
-		$row = [];
-		if ($uid > 0) {
-		    //$contentRepository = $this->objectManager->get('Quizpalme\\Camaliga\\Domain\\Repository\\ConentRepository');
-		    $entry = $this->contentRepository->findOneByUid2($uid);
-		    if ($entry && $entry->getUid()) {
-		        $row['camaliga_title'] = $entry->getTitle();
-		        $row['camaliga_shortdesc'] = $entry->getShortdesc();
-		        $row['camaliga_link'] = $entry->getLink();
-		        $row['camaliga_image'] = $entry->getImage();
-		        $row['camaliga_street'] = $entry->getStreet();
-		        $row['camaliga_zip'] = $entry->getZip();
-		        $row['camaliga_city'] = $entry->getCity();
-		        $row['camaliga_country'] = $entry->getCountry();
-		        $row['camaliga_person'] = $entry->getPerson();
-		        $row['camaliga_phone'] = $entry->getPhone();
-		        $row['camaliga_mobile'] = $entry->getMobile();
-		        $row['camaliga_email'] = $entry->getEmail();
-		        $row['camaliga_latitude'] = $entry->getLatitude();
-		        $row['camaliga_longitude'] = $entry->getLongitude();
-		        $row['camaliga_custom1'] = $entry->getCustom1();
-		        $row['camaliga_custom2'] = $entry->getCustom2();
-		        $row['camaliga_custom3'] = $entry->getCustom3();
-		    }
-     	}
-     	return str_replace(
-     			array_keys($row),
-     			array_values($row),
-     			$param
-     	);
-		//return $this->renderChildren();
+	/**
+	 * Content-ViewHelper
+	 *
+	 * @return string $result
+	 */
+	public function render()
+	{
+	    if ($_GET['tx_camaliga_pi1'])
+	        $camaligaArray = $_GET['tx_camaliga_pi1'];
+	    else if ($_POST['tx_camaliga_pi1'])
+	    	$camaligaArray = $_POST['tx_camaliga_pi1'];
+	    else
+	    	$camaligaArray = array();
+	    $uid = intval($camaligaArray['content']);
+	    //$lang = intval($_GET['L']);
+        $row = [];
+        if ($uid > 0) {
+        	//$contentRepository = $this->objectManager->get('Quizpalme\\Camaliga\\Domain\\Repository\\ConentRepository');
+            $entry = $this->contentRepository->findOneByUid2($uid);
+            if ($entry && $entry->getUid()) {
+            	$row['camaliga_title'] = $entry->getTitle();
+                $row['camaliga_shortdesc'] = $entry->getShortdesc();
+                $row['camaliga_link'] = $entry->getLink();
+                $row['camaliga_image'] = $entry->getImage();
+                $row['camaliga_street'] = $entry->getStreet();
+                $row['camaliga_zip'] = $entry->getZip();
+                $row['camaliga_city'] = $entry->getCity();
+                $row['camaliga_country'] = $entry->getCountry();
+                $row['camaliga_person'] = $entry->getPerson();
+                $row['camaliga_phone'] = $entry->getPhone();
+                $row['camaliga_mobile'] = $entry->getMobile();
+                $row['camaliga_email'] = $entry->getEmail();
+                $row['camaliga_latitude'] = $entry->getLatitude();
+                $row['camaliga_longitude'] = $entry->getLongitude();
+                $row['camaliga_custom1'] = $entry->getCustom1();
+                $row['camaliga_custom2'] = $entry->getCustom2();
+                $row['camaliga_custom3'] = $entry->getCustom3();
+			}
+		}
+		return str_replace(
+			array_keys($row),
+			array_values($row),
+			$this->arguments['param']
+		);
 	}
 }
 ?>
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # camaliga
 
-version 8.4.0
+version 8.4.1
 
 A carousel/gallery/map/list extension that can use the TYPO3 categories and different jQuery-plugins like Slick or Isotope. 
-Bootstrap support. Indexer for ke_search. Many features!
+Bootstrap support. Indexer for ke_search. Many features.
 
 You find the documentation at typo3.org:
 https://docs.typo3.org/typo3cms/extensions/camaliga/
 
-Last feature: documentation updatet.
+Last feature: Content-ViewHelper fixed for TYPO3 9.
