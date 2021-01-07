3e0b8c3fe38bb85778d5947f179fe434762daaca
Kurt Gusbeth <news@quizpalme.de>
2021-01-07 16:47:09 +0100

    version 10.2.3
    unique-check in build slug task.

--- Classes/Task/BuildSlugTask.php
+++ Classes/Task/BuildSlugTask.php
@@ -8,6 +8,7 @@ class BuildSlugTask extends \TYPO3\CMS\Scheduler\Task\AbstractTask {
 	public function execute() {
 		$successfullyExecuted = TRUE;
 		$uids = [];
+		$slugArray = [];
 		
 		$fieldConfig = $GLOBALS['TCA']['tx_camaliga_domain_model_content']['columns']['slug']['config'];
 		$slugHelper = GeneralUtility::makeInstance(\TYPO3\CMS\Core\DataHandling\SlugHelper::class, 'tx_camaliga_domain_model_content', 'slug', $fieldConfig);
@@ -34,6 +35,17 @@ class BuildSlugTask extends \TYPO3\CMS\Scheduler\Task\AbstractTask {
 			if (!$slug) {
     			$slug = $slugHelper->generate($record, $record['pid']);
     			if ($slug) {
+    			    if ($slugArray[$slug]) {
+    			        // Der Slughelper prüft nicht auf unique, also müssen wir das machen
+    			        for ($i = 1; $i < 99; $i++) {
+    			            $tmp = $slug . '-' . $i;
+    			            if (!$slugArray[$tmp]) {
+    			                $slug = $tmp;
+    			                break;
+    			            }
+    			        }
+    			    }
+    			    $slugArray[$slug] = 1;
     			    //echo "neuer Slug: $slug";
         			// Update
         			$queryBuilder = $connection->createQueryBuilder();
@@ -41,6 +53,8 @@ class BuildSlugTask extends \TYPO3\CMS\Scheduler\Task\AbstractTask {
         				$queryBuilder->expr()->eq('uid', $uid)
         			)->set('slug', $slug)->execute();
     			}
+			} else {
+			    $slugArray[$slug] = 1;
 			}
 		}
 		return $successfullyExecuted;
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # camaliga
 
-version 10.2.2
+version 10.2.3
 
 A carousel/gallery/map/list extension that can use the TYPO3 categories and different jQuery-plugins like Slick or Isotope. 
 Bootstrap support. Indexer for ke_search. Many features.
@@ -10,4 +10,4 @@ https://docs.typo3.org/p/quizpalme/camaliga/master/en-us/
 and
 https://docs.typo3.org/p/quizpalme/camaliga/master/de-de/
 
-Last feature: $GLOBALS["TSFE"]->id replaced.
+Last feature: unique-check in build slug task.
